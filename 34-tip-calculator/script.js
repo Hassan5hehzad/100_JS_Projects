f2b931414cdc6f3037128bbeb69f12a6bfe0356d
Hassan Shehzad <hasni6759@gmail.com>
2023-02-25 17:57:12 +0500

    working on tip calculator project, added html css and some javascript

---
+++ 34-tip-calculator/script.js
@@ -0,0 +1,14 @@
+ const btn = document.querySelector(".btn");
+ const tip = document.querySelector(".tip");
+ const total = document.querySelector(".total");
+ const error = document.querySelector(".error");
+
+
+ const calculateTip = () => {
+    const bill = document.querySelector(".bill").value;
+    const rate = document.querySelector(".rate").value;
+
+    if (bill === "" || rate == "") {
+        
+    }
+ }
